fc0416f0114e2d9970e1b617a4c22fecdac82a27
Anderson Mariano <89950546+MarianoAnderson@users.noreply.github.com>
2024-02-28 20:58:19 -0300

    Pasta desnecessaria apagada

--- b63a.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE times (id INTEGER PRIMARY KEY, nome tEXT, vitorias INTEGER);
-
-INSERT INTO times VALUES (1, "Flamengo", 5);
-INSERT INTO times VALUES (2, "Vasco", 3);
-insert into times values (3, "Ceara", 4);
